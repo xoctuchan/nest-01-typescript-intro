a740de4e9abe652e24de7ba903daf2e4ee60fdfe
Juan Pablo Xoc Tuchán <xoctuchan@yahoo.es>
2022-11-24 16:28:00 -0600

    Se completa la sección de intro typescript

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+// import { bulbasaur, pokemons } from './bases/02-objects';
+// import { name, age } from './bases/01-types';
+// import { charmander } from './bases/03-classes';
+// import { charmander } from './bases/04-injection';
+// import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import './style.css';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>Hello ${ charmander.name } ${ charmander.id }!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
